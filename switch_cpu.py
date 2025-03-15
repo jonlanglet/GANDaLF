030cc9ff6590388bc40ed6fd8e95b4e5f76f660c
Jonatan Langlet <jonatan.langlet@gmail.com>
2025-03-15 15:51:30 +0100

    Added attribution for ASCII art (and license)

--- switch_cpu.py
+++ switch_cpu.py
@@ -1,3 +1,27 @@
+"""
+MIT License
+
+Copyright (c) 2025 Jonatan Langlet
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import datetime
 import ipaddress
 import hashlib
@@ -243,6 +267,7 @@ def setDuplicationLevel(egress_port_panel, duplication_level=1):
 	log("Inserting new entry in table...")
 	p4.SwitchIngress.tbl_getDuplicationLevel.add_with_set_duplication_level(egress_port=egress_port_dp, num_duplicates=duplication_level)
 
+#Thanks to Sebastian Stöcker for base ASCII art of Gandalf
 def printArt():
 	log(r"""
 	
